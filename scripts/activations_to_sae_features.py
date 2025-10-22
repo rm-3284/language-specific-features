139289d229f1005cd894e32a09f1c19bdaf611b1
rm-3284 <rm4411@princeton.edu>
2025-10-22 16:51:36 -0400

    changed layer name

--- scripts/activations_to_sae_features.py
+++ scripts/activations_to_sae_features.py
@@ -85,7 +85,7 @@ def parse_args() -> Args:
         "--layer",
         help="layer(s) to be processed. The values should be the path to the layer in the model. Support bracex expansion",
         type=str,
-        default=layer_lists,
+        default=["model.layers.{0..26}.mlp"],
         nargs="+",
     )
 
